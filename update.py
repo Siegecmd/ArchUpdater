2279f13711e50c29fdcbe435a318ff4536462786
siegecmd <kyle@Kyles-MBP.Dlink>
2021-05-22 12:41:09 +0200

    finished i guess

--- update.py
+++ update.py
@@ -1,14 +1,22 @@
-import random
-import time
+import random #random gen
+import os #OS ID
+import time #for sleep
+from halo import Halo #loading
 ## Question
 l1 = ["yes", "no"]
 for x in range(1):
-    rand = random.randint(0, 1)
-    print("\033[1;31;01mIs it time for a pacman -Syu?\r")
-## "calculating"
-    time.sleep(5)
+    update = random.randint(0, 1) # 0 = y 1 = n
+    print("\033[1;31;01mIs it time to update?\r")
+
+with Halo(text='Loading', spinner='dots'):
 ## Output
     print("\033[1;32;01m\n")
-    print(l1[rand])
-
+time.sleep(2)
+print(l1[update])
+time.sleep(5)
 ## if yes -> force update lol
+if update == 0:
+	os.system("sudo pacman -Syu")
+else:
+	print("it's your lucky day.")
+	exit()
